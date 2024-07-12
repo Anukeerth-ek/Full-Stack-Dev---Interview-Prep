92b4313bc72c931b0127589e57e7f5938c698858
Anukeerth-ek <152538146+Anukeerth-ek@users.noreply.github.com>
2024-07-12 17:57:28 +0530

    Added some backend questions

---
+++ Database/Node.js/index.js
@@ -0,0 +1,41 @@
+// ____________________________________________
+// 1. What is Node.js?
+// ans: * Node.js is an open source server environment. 
+//      * Node.js is free.
+//      * Node.js uses JavaScript on the server.
+//      * Node.js runs on various server environment (windows, linux, mac os)
+// ____________________________________________
+
+// ____________________________________________
+//2.  Why Node.js
+// ans: * Node.js uses asynchronous programming.
+//      * Node.js is a single threaded, non-blocking asynchrounous programming that is very memory efficient.
+
+// # The commmon task of a web server can be to open the file in the server and return the content to the client
+// # This is how Php and Asp handle the file requests _______
+// * Sends the task to the computer's file system.
+// * The server will wait file system to open and read the content.
+// * Return the content to the client.
+// * Ready to handle the next file request.
+
+// # This is how node.js handle the file request ________________
+// * Sent the task to the computer's file system.
+// * Ready to handle the next request.
+// * When the file system has opened the file system it will sent the content to the client.
+// ____________________________________________
+
+// ____________________________________________
+// 3. What can node.js do?
+// ans *Node.js can generate dynamic page content
+// *Node.js can create, open, read, write, delete, and close files on the server
+// *Node.js can collect form data
+// *Node.js can add, delete, modify data in your database
+// ____________________________________________
+
+// ____________________________________________
+// 4 What is a Node.js File?
+// ans: * Node.js files contain tasks that will be executed on certain events
+// * A typical event is someone trying to access a port on the server
+// * Node.js files must be initiated on the server before having any effect
+// *Node.js files have extension ".js"
+// ____________________________________________
