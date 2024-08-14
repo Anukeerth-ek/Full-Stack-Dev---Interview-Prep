aeab4bb8e9c561a02ef1a598f07b11ea741f026f
Anukeerth-ek <152538146+Anukeerth-ek@users.noreply.github.com>
2024-08-14 07:14:14 +0530

    Added CSS questions:

---
+++ Front End/CSS.js
@@ -0,0 +1,38 @@
+// _________________________________________________________________
+// 1. Explain a few advantages of CSS.
+// ans:
+// Faster page speed: CSS can help websites load faster.
+// Better user experience: CSS can make web pages more visually appealing and easier to use.
+// Easier maintenance: CSS separates content and style, which can make it easier to maintain web pages and update them.
+// Consistent design: CSS can help ensure a consistent visual identity across a website or multiple pages.
+// _________________________________________________________________
+
+// _________________________________________________________________
+// 2. What is the difference between a class and an ID?
+// ans:
+// The main use of class is we can have multiple class name to multiple tags and we the id should be unique. We only have one id to the one Element.
+// _________________________________________________________________
+
+// _________________________________________________________________
+// 3. Define z-index.
+// ans:
+// Z-index is used to specify the stack order of elements that are overlaping each other. The element with high Z-index will stacked above the lower index element. Its default value is Zero and can take negative and positive number. It take the following value - auto, Number, initial and inherit.
+// _________________________________________________________________
+
+// _________________________________________________________________
+// 4. Define contextual selectors.
+// ans:
+// Contextual selectors are powerful tools for creating more specific and flexible styles based on the structure and relationships of HTML elements. They help you avoid overly generic styles and keep your CSS organized and efficient.
+// _________________________________________________________________
+
+// _________________________________________________________________
+// 5.  How does Calc work?
+// ans:
+// Calc can be used to specify the result of the mathematical operation of two or more elements. For example to specify the width elements by the addition of two or more elements, we can write as
+// eg:
+//  .new-btn {
+//     width: calc(100px - 50px);
+// }
+// _________________________________________________________________
+
+// _________________________________________________________________
