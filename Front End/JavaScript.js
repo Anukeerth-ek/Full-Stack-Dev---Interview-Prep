7c195f632f594eb1dd59d5b12edebbecf0f19103
Anukeerth-ek <152538146+Anukeerth-ek@users.noreply.github.com>
2024-08-22 23:38:23 +0530

    Added js questions:

---
+++ Front End/JavaScript.js
@@ -0,0 +1,9 @@
+// _________________________________________________________________
+// 1. Explain the differences between == and ===
+// ans: 
+// == (Loose Equality): "When using ==, JavaScript performs type coercion, so 5 == '5' evaluates to true because the string '5' is converted to the number 5 before the comparison."
+
+// === (Strict Equality): "When using ===, no type conversion occurs, so 5 === '5' evaluates to false because the number 5 and the string '5' are of different types."
+// _________________________________________________________________
+
+// _________________________________________________________________
